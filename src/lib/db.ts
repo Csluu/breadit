7931e79d1cbcf790450cd4adddc1fa04233f3eca
Chris <csluu@protonmail.com>
2023-09-26 22:01:57 -0400

    Starting files

---
+++ src/lib/db.ts
@@ -0,0 +1,20 @@
+import { PrismaClient } from '@prisma/client'
+import "server-only"
+
+// when in local development it uses the cache one instead of making another instance
+declare global {
+  // eslint-disable-next-line no-var, no-unused-vars
+  var cachedPrisma: PrismaClient
+}
+
+let prisma: PrismaClient
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient()
+} else {
+  if (!global.cachedPrisma) {
+    global.cachedPrisma = new PrismaClient()
+  }
+  prisma = global.cachedPrisma
+}
+
+export const db = prisma
